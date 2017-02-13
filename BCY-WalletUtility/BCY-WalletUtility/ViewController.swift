d638e4bac436e50bbb4484b9401b0c6d5257c176
Rob Enriquez <rob@goabra.com>
2017-02-13 00:33:30 -0800

    replaced pod to alamofire

--- BCY-WalletUtility/BCY-WalletUtility/ViewController.swift
+++ BCY-WalletUtility/BCY-WalletUtility/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
@@ -20,6 +21,30 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    func postCurl() {
+        //GET ACCESS TOKEN
+        let str = "5cee913bf0e04890bb37f7d301192a67"
+        let utf8str = str.data(using: String.Encoding.utf8)
+        //let basic_auth_token = utf8str?.base64EncodedStringWithOptions(NSData.Base64EncodingOptions(rawValue: 0))
+        let basic_auth_token = utf8str?.base64EncodedString(options: NSData.Base64EncodingOptions(rawValue: 0))
+        
+        let headers = ["Accept" : "application/json", "Authorization" :"Basic "+basic_auth_token!]
+        
+        Alamofire.request("https://api.sandbox.paypal.com/v1/oauth2/token",
+                          method: .post, parameters: parameters, encoding:URLEncoding.default,
+                          headers: headers)
+            .responseJSON { response in
+                SwiftLoader.hide()
+                
+                print(response.request as Any)  // original URL request
+                print(response.response as Any) // URL response
+                print(response.result.value as Any)   // result of response serialization
+                
+                
+                
+        }
+    }
 
 }
 
